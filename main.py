4db6fe30473072b5d03b0bd638c19edd08475506
fehervaryy <martindmhe@gmail.com>
2025-05-11 17:00:10 -0400

    commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+def fizzbuzz():
+    for i in range(1, 51):
+        sleep(0.5)
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    fizzbuzz()
